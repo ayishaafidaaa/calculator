7003cac2a93104cdd226cbff9aa964577d2ab52c
ayishaafidaaa <ayishafida52@gmail.com>
2023-08-24 16:35:57 +0530

    calculator

---
+++ calcu.js
@@ -0,0 +1,17 @@
+// display
+function display(num){
+    output.value += num
+}
+
+function  clearAll(){
+    output.value=''
+}
+
+function evaluateExp(){
+    output.value=eval(output.value)
+}
+
+function removeLast(){
+    currentExp=output.value
+    output.value=currentExp.slice(0,-1)
+}
